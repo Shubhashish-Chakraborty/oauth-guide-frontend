edd0fab3f8be932ceb7a6e539510479a552c6441
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-06-06 16:09:32 +0530

    Basic web ready, oauth completely working!

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+    return (
+        <div className="h-screen flex flex-col md:flex-row gap-5 justify-center items-center bg-red-100">
+            <div className="font-extrabold text-xl md:text-5xl">
+                WELCOME TO DASHBOARD!
+            </div>
+        </div>
+    )
+}
